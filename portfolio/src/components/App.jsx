b2aa2c75c03d3d6685a85ef6a4d2ad0b5263c65f
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-09-17 15:08:12 -0300

    atualização

--- portfolio/src/components/App.jsx
+++ portfolio/src/components/App.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import Header from "./Header.jsx";
 import MainHeader from "./MainHeader.jsx";
 import About from "./About.jsx";
+import Skills from "./Skills.jsx";
 
 export default () => {
   return (
     <>
       <MainHeader />
       <About />
+      <Skills />
     </>
   )
 }
