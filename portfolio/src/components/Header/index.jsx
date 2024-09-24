a8085a060b8b3355c40148efaff9a2447982fdb6
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-09-24 14:10:45 -0300

    atualização

--- portfolio/src/components/Header/index.jsx
+++ portfolio/src/components/Header/index.jsx
@@ -1,24 +1,29 @@
 import React from "react";
+import MenuToggle from "./MenuToggle";
 
 export default () => {
     return (
-        <header className="d-none d-md-inline sticky-top">
-            <div className="container">
-                <div className="row d-flex align-items-center py-2">
-                    <div className="col-12 col-md-6">
-                        <h1 className="w-50 m-0 text-white">PORTFÓLIO</h1>
-                    </div>
+        <header className="sticky-top bg-blue-dark">
+            <div className="d-none d-md-inline">
+                <div className="container">
+                    <div className="row d-flex align-items-center py-2">
+                        <div className="col-12 col-md-6">
+                            <h1 className="w-50 m-0 text-white">PORTFÓLIO</h1>
+                        </div>
 
-                    <div className="col-12 col-md-6">
-                        <nav className="d-flex align-items-center justify-content-around">
-                            <p className="m-0 text-white">Home</p>
-                            <p className="m-0 text-white">Sobre</p>
-                            <p className="m-0 text-white">Habilidades</p>
-                            <p className="m-0 text-white">Projetos</p>
-                        </nav>
+                        <div className="col-12 col-md-6">
+                            <nav className="d-flex align-items-center justify-content-around">
+                                <p className="m-0 text-white fw-bold">Home</p>
+                                <p className="m-0 text-white fw-bold">Sobre</p>
+                                <p className="m-0 text-white fw-bold">Habilidades</p>
+                                <p className="m-0 text-white fw-bold">Projetos</p>
+                            </nav>
+                        </div>
                     </div>
                 </div>
             </div>
+
+            <MenuToggle />
         </header>
     )
 }
